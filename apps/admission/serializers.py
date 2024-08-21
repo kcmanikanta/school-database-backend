3662609e424b3d3b33b56f8eca8c9a60f7666b0f
sajid-techis <sajid@techis.io>
2024-08-22 01:07:27 +0530

    Updated Admission Response

--- apps/admission/serializers.py
+++ apps/admission/serializers.py
@@ -2,12 +2,33 @@ from rest_framework import serializers
 from .models import Admission_form
 
 class AdmissionFormSerializer(serializers.ModelSerializer):
-    signature = serializers.ImageField(read_only=False,required=False)
-    photograph = serializers.ImageField(read_only=False,required=False)
+    signature = serializers.ImageField(required=False)
+    photograph = serializers.ImageField(required=False)
 
     class Meta:
         model = Admission_form
         fields = '__all__'
+
+    def create(self, validated_data):
+        # Extract the files from validated data
+        photograph = validated_data.pop('photograph', None)
+        signature = validated_data.pop('signature', None)
+
+        # Create the Admission_form object
+        admission_form = Admission_form.objects.create(**validated_data)
+
+        # If a photograph is provided, save it
+        if photograph:
+            admission_form.photograph = photograph
+            admission_form.save()
+
+        # If a signature is provided, save it
+        if signature:
+            admission_form.signature = signature
+            admission_form.save()
+
+        return admission_form
+
 
 
 class AdmissionListSerializer(serializers.ModelSerializer):
