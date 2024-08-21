14d8d94920325fb42df14a79aca76e77d64152fc
sajid-techis <sajid@techis.io>
2024-08-22 02:20:47 +0530

    Updated Admission Response

--- apps/admission/views.py
+++ apps/admission/views.py
@@ -5,7 +5,17 @@ from .serializers import AdmissionFormSerializer, AdmissionListSerializer
 from apps.users.mixins import CustomLoginRequiredMixin
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
 
+
+from django_filters import rest_framework as filters
+
+class AdmissionFormFilter(filters.FilterSet):
+    id = filters.NumberFilter(field_name='id', lookup_expr='exact')
+
+    class Meta:
+        model = Admission_form
+        fields = ['id', 'firstname', 'lastname']
 # Create your views here.
 
 
@@ -17,6 +27,9 @@ class AdmissionFormView(generics.CreateAPIView):
 class AdmissionListView(generics.ListAPIView):
     queryset = Admission_form.objects.all()
     serializer_class = AdmissionListSerializer
-    search_fields = ['firstname','lastname','id']
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = AdmissionFormFilter
+    search_fields = ['firstname', 'lastname']
+    filterset_fields = ['id']
 
 
