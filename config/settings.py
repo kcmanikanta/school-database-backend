236a81ccadb52e5d48670f84af2237ad849cb855
sajid-techis <sajid@techis.io>
2024-08-27 22:37:23 +0530

    Updated DB

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000','http://localhost:5173', 'https://frontend-electric-views.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000','http://localhost:5173', 'https://gmsk.netlify.app/']
 
 
 
